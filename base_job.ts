ee4a56f530b9e9274192ac4577d8cdd6ef8b9f08
DaiJie <daijie@php.net>
2024-04-01 19:39:38 +0800

    feat: support setting for queueName and jobName

--- base_job.ts
+++ base_job.ts
@@ -1,20 +1,52 @@
 import app from "@adonisjs/core/services/app"
+import { ResqueConfig } from "./types.js"
+
 export default class BaseJob {
 
     delayMs: number = 0
     runAtMs?: number
-    queue?: string
+    /**
+     * the default JobName is this class name  
+     * it **MUST be a unique name**
+     */
+    jobName?: string
+    /**
+     * set a queueName for this job
+     * default configured in `config/resque.ts`
+     */
+    queueName?: string
     args: any[] = []
+    allArgs: any[][] = []
+    hasEnqueued: boolean = false
+    hasEnqueuedAll: boolean = false
 
     static enqueue<T extends typeof BaseJob>(this: T, ...args: Parameters<T['prototype']['perform']>) {
         const job = new this
         return job.enqueue(...args)
     }
     enqueue<T extends BaseJob>(this: T, ...args: Parameters<T['perform']>) {
-        this.queue = this.constructor.name
         this.args = args
+        this.hasEnqueued = true
         return this
     }
+    static queue(queueName: string) {
+        const job = new this
+        return job.queue(queueName)
+    }
+    queue(queueName: string) {
+        this.queueName = queueName
+        return this
+    }
+    static enqueueAll<T extends typeof BaseJob>(this: T, args: Parameters<T['prototype']['perform']>[]) {
+        const job = new this
+        return job.enqueueAll(args)
+    }
+    enqueueAll<T extends BaseJob>(this: T, args: Parameters<T['perform']>[]) {
+        this.allArgs = args
+        this.hasEnqueuedAll = true
+        return this
+    }
+
     static in(ms: number) {
         return (new this).in(ms)
     }
@@ -32,10 +64,29 @@ export default class BaseJob {
     perform(..._args: any[]): any {
 
     }
-    then(fn: (result: boolean) => void) {
-        app.container.make('queue').then((queue) => {
-            queue.enqueue('default', this.constructor.name, this.args).then(fn)
-        })
-        // console.log("then", this.constructor.name, this.runAtMs, this.delayMs, this.args)
+    private async execute() {
+        const resqueConfig = app.config.get<ResqueConfig>('resque')
+        const jobName = this.jobName ?? this.constructor.name
+        const queueName = this.queueName ?? resqueConfig.queueNameForJobs
+        const queue = await app.container.make('queue')
+        if (this.hasEnqueued) {
+            return queue.enqueue(queueName, jobName, this.args)
+        } else if (this.hasEnqueuedAll) {
+            return Promise.all(this.allArgs.map(arg => queue.enqueue(queueName, jobName, arg)))
+        } else {
+            return false
+        }
+
+    }
+    /**
+     * this method runs after an `await` statement
+     * e.g, 
+     * ```typescript
+     * await job.enqueue().in(2000)
+     * ```
+     * @param fn 
+     */
+    then(fn: (result: boolean | boolean[]) => void) {
+        this.execute().then(fn)
     }
 }
--- define_config.ts
+++ define_config.ts
@@ -2,10 +2,14 @@ import type { RedisConnections } from './types.js'
 import { MultiWorker, Worker } from 'node-resque'
 
 export function defineConfig<Connections extends RedisConnections>(config: {
-    redisConnection: keyof Connections;
-    multiWorkerOption: MultiWorker['options']
+    redisConnection: keyof Connections
+    runWorkerInWebEnv: boolean
+    runScheduler: boolean
     isMultiWorkerEnabled: boolean
+    multiWorkerOption: MultiWorker['options']
     workerOption: Worker['options']
+    queueNameForJobs: string
+    queueNameForWorkers: string | string[]
 }) {
     return config
 }
