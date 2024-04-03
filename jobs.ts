3a7af1b6dc83689777e51e57fb57c4476c2ad9ac
DaiJie <daijie@php.net>
2024-04-03 17:12:52 +0800

    fix: should ignore missing jobs dir

--- jobs.ts
+++ jobs.ts
@@ -4,7 +4,9 @@ import Job from "./base_job.js"
 import { NodeResqueJob } from './types.js'
 
 export async function importAllJobs() {
-    const jobs: Record<string, typeof Job> = await fsImportAll(app.makePath('app/jobs'))
+    const jobs: Record<string, typeof Job> = await fsImportAll(app.makePath('app/jobs'), {
+        ignoreMissingRoot: true
+    })
     const Jobs = Object.values(jobs)
     return Jobs.reduce((accumulator, Job) => {
         const job = new Job
