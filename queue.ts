7419c8931a176b4ea9d7d2fd2116bc49de34ef6f
DaiJie <daijie@php.net>
2024-04-03 16:22:06 +0800

    docs: add docs link

--- queue.ts
+++ queue.ts
@@ -2,6 +2,12 @@ import { Queue } from "node-resque";
 import { getConnection } from "./services/main.js";
 import { NodeResqueJob } from "./types.js";
 
+/**
+ * Create a NodeResque Queue
+ * @docs https://github.com/actionhero/node-resque?tab=readme-ov-file#queues
+ * @param jobs 
+ * @returns Queue
+ */
 export function createQueue(jobs: Record<string, NodeResqueJob>) {
     return new Queue({
         connection: getConnection()
