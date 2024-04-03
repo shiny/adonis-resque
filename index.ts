c458420355dbf81ece13117ddaf1911c788f640b
DaiJie <daijie@php.net>
2024-04-03 16:21:25 +0800

    feat: getConfig

--- index.ts
+++ index.ts
@@ -7,5 +7,22 @@ export { Worker } from 'node-resque'
 export { Queue } from 'node-resque'
 export { default as BaseJob } from './base_job.js'
 import { joinToURL } from '@poppinss/utils'
+import { ResqueConfig } from './types.js'
+import app from '@adonisjs/core/services/app'
 
 export const stubsRoot = joinToURL(import.meta.url, 'stubs')
+
+/**
+ * Get a resque config 
+ * @param name key name
+ */
+export function getConfig<key extends keyof ResqueConfig>(name: key): ResqueConfig[key] {
+    return getConfigAll()[name]
+}
+
+/**
+ * Get all resque config 
+ */
+export function getConfigAll() {
+    return app.config.get<ResqueConfig>('resque')
+}
