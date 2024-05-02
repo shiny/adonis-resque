0ded2bf7b43d3bf33c0c4055f90c03b2dd940242
戴劼 <daijie@php.net>
2024-05-03 04:13:53 +0800

    feat: validate the imported jobs

--- jobs.ts
+++ jobs.ts
@@ -4,12 +4,40 @@ import Job from "./base_job.js"
 import { NodeResqueJob } from './types.js'
 
 export async function importAllJobs() {
-    const jobs: Record<string, typeof Job> = await fsImportAll(app.makePath('app/jobs'), {
+    const jobs: Record<string, unknown> = await fsImportAll(app.makePath('app/jobs'), {
         ignoreMissingRoot: true
     })
-    const Jobs = Object.values(jobs)
+    /**
+     * Duck typing check
+     * @param job 
+     * @returns 
+     */
+    const isValidJob = (job: any): job is typeof Job => {
+        if (!job) {
+            return false
+        }
+        if (typeof job?.prototype?.perform !=='function') {
+            return false
+        }
+        if (typeof job?.prototype?.enqueue !== 'function') {
+            return false
+        }
+        if (typeof job?.prototype?.then !== 'function') {
+            return false
+        }
+        return true
+    }
+    const Jobs = Object.values(jobs).filter(isValidJob)
     return Jobs.reduce((accumulator, Job) => {
         const job = new Job
+        if (!Array.isArray(job.plugins)) {
+            job.plugins = []
+        }
+        const plugins = job.plugins.map(([plugin]) => plugin)
+        const pluginOptions = job.plugins.reduce((acc, [plugin, options]) => {
+            acc[plugin.name] = options
+            return acc
+        }, {} as Record<string, any>)
         accumulator[Job.name] = {
             perform: async (...args: any[]) => {
                 try {
@@ -20,11 +48,8 @@ export async function importAllJobs() {
                 }
             },
             job,
-            plugins: job.plugins.map(([plugin]) => plugin),
-            pluginOptions: job.plugins.reduce((acc, [plugin, options]) => {
-                acc[plugin.name] = options
-                return acc
-            }, {} as Record<string, any>)
+            plugins,
+            pluginOptions
         }
         return accumulator
     }, {} as Record<string, NodeResqueJob>)
